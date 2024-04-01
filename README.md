1314c6612bf1046e465c6b4577927f9d78e94211
baizea99 <antoinebaize@gmail.com>
2024-04-01 12:56:50 -0400

    Updated code files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # customer_banking
+
+# Sample Banking and Interest calculator
+
+* This tool uses a basic interest calculation to allow the user to calculate the interest on their banking accounts.
+
+* By allowing the user to input their own information the program can run interest calculations on a wide variety of accounts
+
+* the function structure allows this program to be modularly used for other banking programs with little to no difficulty
--- cd_account.py
+++ cd_account.py
@@ -1,7 +1,6 @@
 """Import the Account class from the Account.py file."""
-# ADD YOUR CODE HERE
-
-def create_cd_account(balance, interest_rate, months):
+from Account import Account
+def create_cd_account(balance, apr, months):
     """Creates a CD account, calculates interest earned, and updates the account balance.
 
     Args:
@@ -14,20 +13,15 @@ def create_cd_account(balance, interest_rate, months):
         And returns the interest earned.
     """
     # Create an instance of the `Account` class and pass in the balance and interest parameters.
-    #  Hint: You need to add the interest as a value, i.e, 0.
-    # ADD YOUR CODE HERE
-
+    interest = 0
+    cd_account = Account(balance, interest)
     # Calculate interest earned
-    # ADD YOUR CODE HERE
-
+    interest_earned = balance * (apr / 100 * months/12)
     # Update the CD account balance by adding the interest earned
-    # ADD YOUR CODE HERE
-
+    new_balance = balance + interest_earned
     # Pass the updated_balance to the set balance method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
-
+    cd_account.set_balance(new_balance)
     # Pass the interest_earned to the set interest method using the instance of the CDAccount class.
-    # ADD YOUR CODE HERE
-
+    cd_account.set_interest(interest_earned)
     # Return the updated balance and interest earned.
-    return  # ADD YOUR CODE HERE
+    return  new_balance, interest_earned
